d22282a0915a9276a6cbad14790e01f95abf6914
Patrick Martin <patrickwaltermartin@gmail.com>
2018-06-05 13:59:30 -0400

    Added code to pass hasLowerCase test

--- app/src/main/java/com/example/patrick/passsword_validator/Validator.java
+++ app/src/main/java/com/example/patrick/passsword_validator/Validator.java
@@ -6,17 +6,22 @@ public class Validator {
             return false;
 
         boolean upperCaseFlag = false;
+        boolean lowerCaseFlag = false;
         boolean numberFlag = false;
 
         for(int i = 0; i<password.length();i++){
            char testChar = password.charAt(i);
            if(Character.isUpperCase(testChar)== true)
                upperCaseFlag = true;
-            if(Character.isDigit(testChar)== true)
+
+           if(Character.isDigit(testChar)== true)
                 numberFlag = true;
+
+           if(Character.isLowerCase(testChar) == true)
+               lowerCaseFlag = true;
         }
 
-        if(upperCaseFlag == false || numberFlag == false)
+        if(upperCaseFlag == false || numberFlag == false || lowerCaseFlag == false)
             return false;
 
         return true;
