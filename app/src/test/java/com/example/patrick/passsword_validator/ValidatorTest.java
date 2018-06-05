6b05c3977aa69f64ab1a264e41492d55d8154a3c
Patrick Martin <patrickwaltermartin@gmail.com>
2018-06-05 13:48:48 -0400

    Added test to check if the password has at least one number

--- app/src/test/java/com/example/patrick/passsword_validator/ValidatorTest.java
+++ app/src/test/java/com/example/patrick/passsword_validator/ValidatorTest.java
@@ -8,20 +8,25 @@ public class ValidatorTest {
     @Test
     public void isPasswordTest(){
         assertTrue(!Validator.validate("password"));
-        assertTrue(Validator.validate("Thisshouldpass"));
+        assertTrue(Validator.validate("Thisshouldpass1"));
         assertTrue(!Validator.validate("PASSWORD"));
         assertTrue(!Validator.validate("pAsSWoRd"));
     }
 
     @Test
     public void isLessThan8Test(){
-        assertTrue(!Validator.validate("1234"));
+        assertTrue(!Validator.validate("A234"));
         assertTrue(Validator.validate("12B45678"));
     }
 
     @Test
     public void hasCapital(){
         assertTrue(!Validator.validate("12345678"));
+        assertTrue(Validator.validate("A1234567"));
+    }
+    @Test
+    public void hasNumber(){
+        assertTrue(!Validator.validate("Alahkdhflahfalkf"));
         assertTrue(Validator.validate("A1234567"));
     }
 
