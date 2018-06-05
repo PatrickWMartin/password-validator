9814bd89f67deef79020ae3bdd0ca723e7db7979
Patrick Martin <patrickwaltermartin@gmail.com>
2018-06-05 13:55:08 -0400

    Added test for checking if password has a lowercase character

--- app/src/test/java/com/example/patrick/passsword_validator/ValidatorTest.java
+++ app/src/test/java/com/example/patrick/passsword_validator/ValidatorTest.java
@@ -15,19 +15,24 @@ public class ValidatorTest {
 
     @Test
     public void isLessThan8Test(){
-        assertTrue(!Validator.validate("A234"));
-        assertTrue(Validator.validate("12B45678"));
+        assertTrue(!Validator.validate("Ab234"));
+        assertTrue(Validator.validate("12Bb5678"));
     }
 
     @Test
     public void hasCapital(){
         assertTrue(!Validator.validate("12345678"));
-        assertTrue(Validator.validate("A1234567"));
+        assertTrue(Validator.validate("Ab234567"));
     }
     @Test
     public void hasNumber(){
         assertTrue(!Validator.validate("Alahkdhflahfalkf"));
-        assertTrue(Validator.validate("A1234567"));
+        assertTrue(Validator.validate("Ab234567"));
     }
 
+    @Test
+    public void hasLowerCase(){
+        assertTrue(!Validator.validate("A1AAAAAAAAAAA"));
+        assertTrue(Validator.validate("Ab234567"));
+    }
 }
