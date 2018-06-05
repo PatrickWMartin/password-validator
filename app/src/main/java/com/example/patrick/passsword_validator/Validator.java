31f22937dbecea90e96876021850e38294d9bcbe
Patrick Martin <patrickwaltermartin@gmail.com>
2018-06-05 11:33:07 -0400

    Added code to pass the length test

--- app/src/main/java/com/example/patrick/passsword_validator/Validator.java
+++ app/src/main/java/com/example/patrick/passsword_validator/Validator.java
@@ -2,8 +2,9 @@ package com.example.patrick.passsword_validator;
 
 public class Validator {
     public static boolean validate(String password){
-        if(password.toLowerCase().equals("password"))
+        if(password.toLowerCase().equals("password") || password.length() < 8)
             return false;
+
 
         return true;
     }
