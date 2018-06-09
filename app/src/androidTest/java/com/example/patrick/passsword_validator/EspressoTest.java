71266f45a4a7ff7aba9c5fd95a50f84488bade45
Patrick Martin <patrickwaltermartin@gmail.com>
2018-06-09 11:03:46 -0400

    Added more test to check if when a valid password is used the UI acts correctly

--- app/src/androidTest/java/com/example/patrick/passsword_validator/EspressoTest.java
+++ app/src/androidTest/java/com/example/patrick/passsword_validator/EspressoTest.java
@@ -13,6 +13,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -36,6 +38,12 @@ public class EspressoTest {
     @Test
     public void findStrengthText(){
         onView(withId(R.id.strengthText));
+    }
+    @Test
+    public void strongPasswordTest(){
+        onView(withId(R.id.passwordField)).perform(typeText("Thisshouldpass1"));
+        onView(withId(R.id.validateButton)).perform(click());
+        onView(withId(R.id.strengthText)).check(matches(withText("Strong")));
     }
 
 
